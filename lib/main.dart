4682c1e9c64920d1ef68be07b8862ef3dc2beb13
shakib08 <shakibimtiazjishan1111@gmail.com>
2024-09-03 04:28:05 +0600

    Added state

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:task/Pages/home.dart';
 import 'package:task/Pages/login.dart';
 import 'firebase_options.dart';
 void main() async{
@@ -21,8 +23,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: login(),
+        body: AuthWrapper(),
       ),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+         
+
+
+        
+         else if (snapshot.hasData) {
+          
+            return home(); 
+        } else {
+          return login(); // Replace with your actual login page
+        }
+      },
     );
   }
 }
