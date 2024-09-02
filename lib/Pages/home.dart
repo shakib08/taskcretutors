5f30f908b308ec5884d568c1674f24ae2475ed6c
shakib08 <shakibimtiazjishan1111@gmail.com>
2024-09-03 03:09:08 +0600

    Home Page Has been done

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:task/Pages/login.dart';
+import 'package:task/Pages/profile.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -11,6 +15,101 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CareTutors"),
+      ),
+      drawer: const NavigationDrawer(),
+    );
+  }
+}
+
+class NavigationDrawer extends StatefulWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+  @override
+  _NavigationDrawerState createState() => _NavigationDrawerState();
+}
+
+class _NavigationDrawerState extends State<NavigationDrawer> {
+  User? user = FirebaseAuth.instance.currentUser;
+  String userName = "";
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(user!.uid)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+
+                var userData = snapshot.data!.data()
+                    as Map<String, dynamic>?; // Explicit cast
+
+                return Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      radius: 40,
+                    ),
+                    Text(
+                      '${userData?['name'] ?? "Name not available"}',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: size.width * 0.05),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              // Navigate to Home page
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+            onTap: () {
+              // Navigate to Profile page
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => profile()));
+              // Implement navigation to profile page here
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            onTap: () async {
+              // Implement logout functionality here
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => login(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:task/Pages/login.dart';
 import 'firebase_options.dart';
 void main() async{
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
+  runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
